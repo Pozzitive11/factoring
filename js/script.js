c2c654208e67a48cd5e78b41b7da193238112740
Palahin Vladyslav <palahinvlad@gmail.com>
2024-05-31 15:37:47 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -9,35 +9,36 @@ document.addEventListener("DOMContentLoaded", () => {
   // NAVIGATION
 
   const title = document.querySelector("title");
-  const currentPath = window.location.pathname.replace(/\.html$/, "");
+  const currentPath = window.location.pathname.replace(/\.html$/, "").replace(/^\//, "");
+
   const pageTitleMap = {
-    "/index": "ТОВ «КАМПСІС ЛІГАЛ»",
-    "/about-company": "Про компанію",
-    "/contacts": "Контакти",
-    "/documents": "Документи",
-    "/loani": "КТІК (Лоані)",
-    "/ukr-kredit-finance": "УКР КРЕДИТ ФІНАНС",
-    "/kachay-groshi": "Качай гроші",
-    "/avans-kredit": "Аванс кредит",
+    "index": "ТОВ «КАМПСІС ЛІГАЛ»",
+    "about-company": "Про компанію",
+    "contacts": "Контакти",
+    "documents": "Документи",
+    "loani": "КТІК (Лоані)",
+    "ukr-kredit-finance": "УКР КРЕДИТ ФІНАНС",
+    "kachay-groshi": "Качай гроші",
+    "avans-kredit": "Аванс кредит",
   };
 
   title.textContent = pageTitleMap[currentPath] || title.textContent;
 
   document.querySelectorAll(".nav__link").forEach((link) => {
-    const href = link.getAttribute("href").replace(/\.html$/, "");
+    const href = link.getAttribute("href").replace(/\.html$/, "").replace(/^\//, "");
     if (currentPath === href) {
       link.classList.add("active");
     } else {
       link.classList.remove("active");
     }
   });
+
   // ACCORDIONS
   const accordionButtons = document.querySelectorAll(".documents__accordion");
 
   accordionButtons.forEach((accordion) => {
-    accordion.addEventListener("click", function ()  {
+    accordion.addEventListener("click", () => {
       this.classList.toggle("documents__accordion--active");
-      console.log(this.nextElementSibling);
       const documentsList = this.nextElementSibling;
       documentsList.classList.toggle("documents__list--active");
     });
