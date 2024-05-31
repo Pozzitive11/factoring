a119a10105ae9e54ae8daa30ca9f4a1bda290e85
Palahin Vladyslav <palahinvlad@gmail.com>
2024-05-31 14:53:01 +0300

    123

--- js/script.js
+++ js/script.js
@@ -1,54 +1,45 @@
-// BURGER MENU
-const menuBtn = document.querySelector(".burger");
-const menu = document.querySelector(".nav__list");
-menuBtn.addEventListener("click", function () {
-  menuBtn.classList.toggle("show");
-  menu.classList.toggle("show");
-});
+document.addEventListener("DOMContentLoaded", () => {
+  // BURGER MENU
+  const menuBtn = document.querySelector(".burger");
+  const menu = document.querySelector(".nav__list");
+  menuBtn.addEventListener("click", function () {
+    menuBtn.classList.toggle("show");
+    menu.classList.toggle("show");
+  });
+  // NAVIGATION
 
-// NAVIGATION
-const title = document.querySelector("title");
-const currentPath = window.location.pathname;
-const navItems = document.querySelectorAll(".nav__item");
-const currentPathWithoutExtension = currentPath.replace(/\.html$/, "");
+  const title = document.querySelector("title");
+  const currentPath = window.location.pathname.replace(/\.html$/, "");
+  const pageTitleMap = {
+    "/index": "ТОВ «КАМПСІС ЛІГАЛ»",
+    "/about-company": "Про компанію",
+    "/contacts": "Контакти",
+    "/documents": "Документи",
+    "/loani": "КТІК (Лоані)",
+    "/ukr-kredit-finance": "УКР КРЕДИТ ФІНАНС",
+    "/kachay-groshi": "Качай гроші",
+    "/avans-kredit": "Аванс кредит",
+  };
 
-const pageTitleMap = {
-  "/index": "ТОВ «КАМПСІС ЛІГАЛ»",
-  "/about-company": "Про компанію",
-  "/contacts": "Контакти",
-  "/documents": "Документи",
-  "/loani": "КТІК (Лоані)",
-  "/ukr-kredit-finance": "УКР КРЕДИТ ФІНАНС",
-  "/kachay-groshi": "Качай гроші",
-  "/avans-kredit": "Аванс кредит",
-};
+  title.textContent = pageTitleMap[currentPath] || title.textContent;
 
-navItems.forEach(function (item) {
-  const link = item.querySelector(".nav__link");
-  link.classList.remove("active");
-  const href = link.getAttribute("href");
-  const hrefWithoutExtension = href.replace(/\.html$/, "");
-
-  if (currentPathWithoutExtension === hrefWithoutExtension) {
-    link.classList.add("active");
-
-    const pageTitle = pageTitleMap[currentPathWithoutExtension] || "";
-    title.textContent = pageTitle;
-  }
-});
-
-// ACCORDIONS
-var acc = document.getElementsByClassName("documents__accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("documents__accordion--active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
+  document.querySelectorAll(".nav__link").forEach((link) => {
+    const href = link.getAttribute("href").replace(/\.html$/, "");
+    if (currentPath === href) {
+      link.classList.add("active");
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      link.classList.remove("active");
     }
   });
-}
+  // ACCORDIONS
+  const accordionButtons = document.querySelectorAll(".documents__accordion");
+
+  accordionButtons.forEach((accordion) => {
+    accordion.addEventListener("click", function ()  {
+      this.classList.toggle("documents__accordion--active");
+      console.log(this.nextElementSibling);
+      const documentsList = this.nextElementSibling;
+      documentsList.classList.toggle("documents__list--active");
+    });
+  });
+});
